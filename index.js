39dd2a34678f2d73bcb9b5e51424f9176a9e0d0a
Mukesh Suthar <mukeshsuthar336@gmail.com>
2020-01-11 14:52:32 +0530

    Updates:
    - Add function to  get top n words
    - Add Eslint

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 /* eslint-disable linebreak-style */
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors')
-
+const cors = require('cors');
 const words = require('./routes/words');
 const logger = require('./logger');
 require('dotenv').config();
@@ -11,17 +10,17 @@ const app = express();
 global.logger = logger;
 
 // register middlewares
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
+    'Access-Control-Allow-Headers',
+    'Origin,Content-Type,Content-Length, Authorization, Accept,X-Requested-With'
   );
-  res.header("Access-Control-Allow-Methods", "POST,GET,DELETE");
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'POST,GET,DELETE');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
--- routes/words.js
+++ routes/words.js
@@ -1,26 +1,54 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-undef */
+/* eslint-disable array-callback-return */
 const express = require('express');
+const axios = require('axios');
+
 const wordroute = express.Router();
+
+getTopNwords = async (topn) => {
+  const mapwordcount = {};
+  const finaltopcount = [];
+
+  const response = await axios.get('https://terriblytinytales.com/test.txt');
+  const text = response.data;
+
+  text.match(/[A-za-z0-9_']+/g).map((word) => {
+    const lowerword = word.toLowerCase();
+    mapwordcount[lowerword] = (mapwordcount[lowerword]) ? mapwordcount[lowerword] + 1 : 1;
+  });
+
+  topkeys = Object.keys(mapwordcount)
+    .sort((a, b) => mapwordcount[a] - mapwordcount[b])
+    .reverse()
+    .slice(0, topn)
+    .map((word) => {
+      finaltopcount.push({ word, count: mapwordcount[word] });
+    });
+
+  return finaltopcount;
+};
 
 wordroute.post('/wordfreq/:topn', async (req, res) => {
   try {
-      if(req.params.topn){
-        logger.info(`Routes/Words: Params: Get top most ${req.params.topn} frequent words`);
-        // getTopN(req.params.topn)
-        res.status(200).send({data:'topnwords'});
-      }else if(Object.keys(req.body).length>0){
-        logger.info(`Routes/Words: Body: Get top most ${req.body.topn} frequent words`);
-        // getTopN(req.body.topn)
-        res.status(200).send({data:'topnwords'});
-      }else{
-        logger.warn(`Routes/Words: topn variable not found in params or body`)
-        res.status(401).send({message:'topn variable not found in params or body'});
-      }
+    if (req.params.topn) {
+      // Get top n number from params
+      logger.info(`Routes/Words: Params: Get top most ${req.params.topn} frequent words`);
+      const finaldata = await getTopNwords(req.params.topn);
+      res.status(200).send({ message: 'success', data: finaldata });
+    } else if (Object.keys(req.body).length > 0) {
+      // Get top n number from body
+      logger.info(`Routes/Words: Body: Get top most ${req.body.topn} frequent words`);
+      const finaldata = getTopNwords(req.body.topn);
+      res.status(200).send({ message: 'success', data: finaldata });
+    } else {
+      logger.warn('Routes/Words: topn variable not found in params or body');
+      res.status(401).send({ message: 'topn variable not found in params or body' });
+    }
   } catch (error) {
     logger.error(`Routes/Words: Error while finding top frequent words: ${((error.stack) ? error.stack : error)}`);
-    res.status(401).send({message:`Error while finding top frequent words: ${error}`});
+    res.status(401).send({ message: `Error while finding top frequent words: ${error}` });
   }
 });
 
